6ba2b08e05cf77bab12fa494eaec30dbec6d64e8
rojson16 <150377250+rojson16@users.noreply.github.com>
2023-11-14 10:25:55 +0100

    Add files via upload

---
+++ zadania dla pana/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        FirstClass obj1 = new FirstClass(1, 2, 3, 4, 5, 6, 7, 8);
+        FirstClass obj2 = new FirstClass(9, 10, 11, 12, 13, 14, 15, 16);
+        FirstClass obj3 = new FirstClass(17, 18, 19, 20, 21, 22, 23, 24);
+
+        SecondClass objA1 = new SecondClass(25, 26, 27, 28, 29, 30, 31, 32);
+        SecondClass objA2 = new SecondClass(33, 34, 35, 36, 37, 38, 39, 40);
+        SecondClass objA3 = new SecondClass(41, 42, 43, 44, 45, 46, 47, 48);
+
+        ThirdClass objY1 = new ThirdClass(49, 50, 51, 52, 53, 54, 55, 56);
+        ThirdClass objY2 = new ThirdClass(57, 58, 59, 60, 61, 62, 63, 64);
+        ThirdClass objY3 = new ThirdClass(65, 66, 67, 68, 69, 70, 71, 72);
+
+        functionality1(obj1);
+        functionality2(objA2);
+        functionality3(objY3);
+    }
+
+    private static void functionality1(FirstClass obj) {
+    }
+
+    private static void functionality2(SecondClass obj) {
+    }
+
+    private static void functionality3(ThirdClass obj) {
+
+    }
+}
---
+++ zadania dla pana/src/SecondClass.java
@@ -0,0 +1,39 @@
+class SecondClass {
+    private int field1;
+    private int field2;
+    private int field3;
+    private int field4;
+    private int field5;
+    private int field6;
+    private int field7;
+    private int field8;
+
+    public SecondClass(int param1, int param2, int param3, int param4, int param5, int param6, int param7, int param8) {
+        this.field1 = param1;
+        this.field2 = param2;
+        this.field3 = param3;
+        this.field4 = param4;
+        this.field5 = param5;
+        this.field6 = param6;
+        this.field7 = param7;
+        this.field8 = param8;
+    }
+
+    public int getField1() {
+        return field1;
+    }
+
+    public int getField2() {
+        return field2;
+    }
+
+
+    public void setField1(int value) {
+        this.field1 = value;
+    }
+
+    public void setField2(int value) {
+        this.field2 = value;
+    }
+
+}
